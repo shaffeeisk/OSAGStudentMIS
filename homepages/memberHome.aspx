2ec8360755b5a7f4006f8472b99160823af29a26
Emily Rohde <rohdeek@dukes.jmu.edu>
2022-03-23 13:11:59 -0400

    trying to import home pages

---
+++ homepages/memberHome.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/memberTemplate.Master" AutoEventWireup="true" CodeBehind="memberHome.aspx.cs" Inherits="Lab2.memberHome" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <br />
+    <asp:Label ID="lblAccessDenied" runat="server" Text=""></asp:Label>
+    <br />
+    <br />
+    <asp:Button ID="btnViewMember" runat="server" Text="View my profile"
+        OnClick="btnViewMember_Click" />
+    <br />
+    <br />
+    <asp:Button ID="btnViewStudents" runat="server" Text="View Student Profiles"
+        OnClick="btnViewStudents_Click" />
+    <br />
+    <br />
+    <asp:Button ID="btnViewScholarships" runat="server" Text="View Available Scholarships"
+        OnClick="btnViewScholarships_Click" />
+    <br />
+    <br />
+    <asp:Button ID="btnViewInternships" runat="server" Text="View Available Internships"
+        OnClick="btnViewInternships_Click" />
+    <br />
+    <br />
+    <asp:Button ID="btnViewJobs" runat="server" Text="View Available Jobs" OnClick="btnViewJobs_Click" />
+    <br />
+    <br />
+    <asp:Button ID="btnApproveRegistration" runat="server" Text="Approve New User Registration"
+        OnClick="btnApproveRegistration_Click" Enabled="false" />
+</asp:Content>
---
+++ homepages/memberHome.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+// colored response import
+using System.Drawing;
+
+namespace Lab2
+{
+    public partial class memberHome : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["AccessDenied"] != null)
+            {
+                lblAccessDenied.Text = Session["AccessDenied"].ToString();
+                lblAccessDenied.ForeColor = Color.Red;
+                lblAccessDenied.Font.Bold = true;
+            }
+            if ((String)Session["Username"] == "admin")
+            {
+                btnApproveRegistration.Enabled = true;
+            }
+        }
+
+        protected void btnViewMember_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("viewMemberProfile.aspx");
+        }
+
+        protected void btnViewScholarships_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("viewTableScholarship.aspx");
+        }
+
+        protected void btnViewInternships_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("viewTableInternship.aspx");
+        }
+
+        protected void btnViewJobs_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("viewTableJob.aspx");
+        }
+
+        protected void btnViewStudents_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("viewTableStudent.aspx");
+        }
+
+        protected void btnApproveRegistration_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("registrationApproval.aspx");
+        }
+    }
+}
