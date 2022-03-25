002f3ad4c3d47097b6a2a8c4cb76af2280fd3e2f
Shaffee Iskandar <shaffeeisk@gmail.com>
2022-03-25 03:07:14 -0400

    fixed 2

--- OSAG/opportunities/Opportunities.aspx.cs
+++ OSAG/opportunities/Opportunities.aspx.cs
@@ -16,7 +16,7 @@ namespace OSAG.opportunities
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            Session["Username"] = "jcrew";
+            
         }
 
         protected void btnView_Click(object sender, EventArgs e)
