06de2a19f540a3b410a3d6e8b97d053d21c3d601
Shaffee Iskandar <shaffeeisk@gmail.com>
2022-03-30 14:42:50 -0400

    try/catch redirects nullreferenceexception when loading page without  req session var

--- OSAG/jobs/JobDetails.aspx.cs
+++ OSAG/jobs/JobDetails.aspx.cs
@@ -16,38 +16,47 @@ namespace OSAG.jobs
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!IsPostBack) //check if the webpage is loaded for the first time.
+            try
             {
-                ViewState["PreviousPage"] =
-            Request.UrlReferrer;//Saves the Previous page url in ViewState
+                if (!IsPostBack) //check if the webpage is loaded for the first time.
+                {
+                    ViewState["PreviousPage"] =
+                Request.UrlReferrer;//Saves the Previous page url in ViewState
+                }
+                //Query
+                String sqlQuery = "Select 'Job Name: ' + JobName as Name, 'Company Name: ' + CompanyName as Company, 'Job Description: ' +  JobDescription as Description, 'Application Deadline: ' + CAST(ApplicationDeadline AS varchar) as Deadline" +
+                    ", 'Start Date: ' + CAST(StartDate as varchar) as Start, 'Weekly Hours: ' + CAST(WeeklyHours AS varchar) as Hours, 'Payment: ' + FORMAT(Payment,'C') as Payment" +
+                    " from Job LEFT JOIN Company ON Company.CompanyID = Job.CompanyID where JobName+CompanyName = '" + Session["View"].ToString()
+                    + "'";
+
+                // Define the Connection
+                SqlConnection sqlConnection = new SqlConnection(WebConfigurationManager.ConnectionStrings["OSAG"].ConnectionString);
+
+                //Create and Format the Command
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandType = CommandType.Text;
+                sqlCommand.CommandText = sqlQuery;
+
+                // Execute the Query and get results
+                sqlConnection.Open();
+                SqlDataReader queryResults = sqlCommand.ExecuteReader();
+                while (queryResults.Read())
+                {
+                    lblDetails.Text = queryResults["Name"].ToString();
+                    lblDetails1.Text = queryResults["Company"].ToString();
+                    lblDetails2.Text = queryResults["Description"].ToString();
+                    lblDetails3.Text = queryResults["Deadline"].ToString();
+                    lblDetails4.Text = queryResults["Start"].ToString();
+                    lblDetails5.Text = queryResults["Hours"].ToString();
+                    lblDetails6.Text = queryResults["Payment"].ToString();
+                }
             }
-            //Query
-            String sqlQuery = "Select 'Job Name: ' + JobName as Name, 'Company Name: ' + CompanyName as Company, 'Job Description: ' +  JobDescription as Description, 'Application Deadline: ' + CAST(ApplicationDeadline AS varchar) as Deadline" +
-                ", 'Start Date: ' + CAST(StartDate as varchar) as Start, 'Weekly Hours: ' + CAST(WeeklyHours AS varchar) as Hours, 'Payment: ' + FORMAT(Payment,'C') as Payment" +
-                " from Job LEFT JOIN Company ON Company.CompanyID = Job.CompanyID where JobName+CompanyName = '" + Session["View"].ToString()
-                + "'";
-
-            // Define the Connection
-            SqlConnection sqlConnection = new SqlConnection(WebConfigurationManager.ConnectionStrings["OSAG"].ConnectionString);
-
-            //Create and Format the Command
-            SqlCommand sqlCommand = new SqlCommand();
-            sqlCommand.Connection = sqlConnection;
-            sqlCommand.CommandType = CommandType.Text;
-            sqlCommand.CommandText = sqlQuery;
-
-            // Execute the Query and get results
-            sqlConnection.Open();
-            SqlDataReader queryResults = sqlCommand.ExecuteReader();
-            while (queryResults.Read())
+            catch (NullReferenceException)
             {
-                lblDetails.Text = queryResults["Name"].ToString();
-                lblDetails1.Text = queryResults["Company"].ToString();
-                lblDetails2.Text = queryResults["Description"].ToString();
-                lblDetails3.Text = queryResults["Deadline"].ToString();
-                lblDetails4.Text = queryResults["Start"].ToString();
-                lblDetails5.Text = queryResults["Hours"].ToString();
-                lblDetails6.Text = queryResults["Payment"].ToString();
+                Session["MustLogin"] = "You must log in to access that page.";
+                Response.Redirect("/login/LoginPage.aspx");
+                throw;
             }
         }
 
--- OSAG/opportunities/OpportunityDetails.aspx.cs
+++ OSAG/opportunities/OpportunityDetails.aspx.cs
@@ -16,32 +16,41 @@ namespace OSAG.opportunities
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!IsPostBack)
+            try
             {
-                ViewState["PreviousPage"] =
-            Request.UrlReferrer;
+                if (!IsPostBack)
+                {
+                    ViewState["PreviousPage"] =
+                Request.UrlReferrer;
+                }
+                //Query
+                String sqlQuery = "Select 'Award Name: ' + AwardName as Name, 'Award Amount: ' +  FORMAT(AwardAmount,'C') as Amount, 'Award Description: ' + AwardDescription as Description from Opportunity where AwardName = '" + Session["View"].ToString()
+                    + "'";
+
+                // Define the Connection
+                SqlConnection sqlConnection = new SqlConnection(WebConfigurationManager.ConnectionStrings["OSAG"].ConnectionString);
+
+                //Create and Format the Command
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandType = CommandType.Text;
+                sqlCommand.CommandText = sqlQuery;
+
+                // Execute the Query and get results
+                sqlConnection.Open();
+                SqlDataReader queryResults = sqlCommand.ExecuteReader();
+                while (queryResults.Read())
+                {
+                    lblDetails.Text = queryResults["Name"].ToString();
+                    lblDetails2.Text = queryResults["Amount"].ToString();
+                    lblDetails3.Text = queryResults["Description"].ToString();
+                }
             }
-            //Query
-            String sqlQuery = "Select 'Award Name: ' + AwardName as Name, 'Award Amount: ' +  FORMAT(AwardAmount,'C') as Amount, 'Award Description: ' + AwardDescription as Description from Opportunity where AwardName = '" + Session["View"].ToString() 
-                + "'";
-
-            // Define the Connection
-            SqlConnection sqlConnection = new SqlConnection(WebConfigurationManager.ConnectionStrings["OSAG"].ConnectionString);
-
-            //Create and Format the Command
-            SqlCommand sqlCommand = new SqlCommand();
-            sqlCommand.Connection = sqlConnection;
-            sqlCommand.CommandType = CommandType.Text;
-            sqlCommand.CommandText = sqlQuery;
-
-            // Execute the Query and get results
-            sqlConnection.Open();
-            SqlDataReader queryResults = sqlCommand.ExecuteReader();
-            while (queryResults.Read()) 
+            catch (NullReferenceException)
             {
-                lblDetails.Text = queryResults["Name"].ToString();
-                lblDetails2.Text = queryResults["Amount"].ToString();
-                lblDetails3.Text = queryResults["Description"].ToString();
+                Session["MustLogin"] = "You must log in to access that page.";
+                Response.Redirect("/login/LoginPage.aspx");
+                throw;
             }
 
         }
