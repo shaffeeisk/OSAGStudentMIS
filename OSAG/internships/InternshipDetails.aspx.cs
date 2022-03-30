d27ea7b03233186bbd3f14a3674d8e7218dd58ca
Shaffee Iskandar <shaffeeisk@gmail.com>
2022-03-30 14:41:03 -0400

    added try/catch to prevent crashing on load without login

--- OSAG/internships/InternshipDetails.aspx.cs
+++ OSAG/internships/InternshipDetails.aspx.cs
@@ -16,38 +16,47 @@ namespace OSAG.internships
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!IsPostBack) //check if the webpage is loaded for the first time.
+            try
             {
-                ViewState["PreviousPage"] =
-            Request.UrlReferrer;//Saves the Previous page url in ViewState
+                if (!IsPostBack) //check if the webpage is loaded for the first time.
+                {
+                    ViewState["PreviousPage"] =
+                Request.UrlReferrer;//Saves the Previous page url in ViewState
+                }
+                //Query
+                String sqlQuery = "Select 'Internship Name: ' + InternshipName as Name, 'Company Name: ' + CompanyName as Company, 'Internship Description: ' +  InternshipDescription as Description, 'Application Deadline: ' + CAST(ApplicationDeadline AS varchar) as Deadline" +
+                    ", 'Start Date: ' + CAST(StartDate as varchar) as Start, 'Weekly Hours: ' + CAST(WeeklyHours AS varchar) as Hours, 'Payment: ' + FORMAT(Payment,'C') as Payment" +
+                    " from Internship LEFT JOIN Company ON Company.CompanyID = Internship.CompanyID where InternshipName + CompanyName = '" + Session["View"].ToString()
+                    + "'";
+
+                // Define the Connection
+                SqlConnection sqlConnection = new SqlConnection(WebConfigurationManager.ConnectionStrings["OSAG"].ConnectionString);
+
+                //Create and Format the Command
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandType = CommandType.Text;
+                sqlCommand.CommandText = sqlQuery;
+
+                // Execute the Query and get results
+                sqlConnection.Open();
+                SqlDataReader queryResults = sqlCommand.ExecuteReader();
+                while (queryResults.Read())
+                {
+                    lblDetails.Text = queryResults["Name"].ToString();
+                    lblDetails1.Text = queryResults["Company"].ToString();
+                    lblDetails2.Text = queryResults["Description"].ToString();
+                    lblDetails3.Text = queryResults["Deadline"].ToString();
+                    lblDetails4.Text = queryResults["Start"].ToString();
+                    lblDetails5.Text = queryResults["Hours"].ToString();
+                    lblDetails6.Text = queryResults["Payment"].ToString();
+                }
             }
-            //Query
-            String sqlQuery = "Select 'Internship Name: ' + InternshipName as Name, 'Company Name: ' + CompanyName as Company, 'Internship Description: ' +  InternshipDescription as Description, 'Application Deadline: ' + CAST(ApplicationDeadline AS varchar) as Deadline" +
-                ", 'Start Date: ' + CAST(StartDate as varchar) as Start, 'Weekly Hours: ' + CAST(WeeklyHours AS varchar) as Hours, 'Payment: ' + FORMAT(Payment,'C') as Payment" +
-                " from Internship LEFT JOIN Company ON Company.CompanyID = Internship.CompanyID where InternshipName + CompanyName = '" + Session["View"].ToString()
-                + "'";
-
-            // Define the Connection
-            SqlConnection sqlConnection = new SqlConnection(WebConfigurationManager.ConnectionStrings["OSAG"].ConnectionString);
-
-            //Create and Format the Command
-            SqlCommand sqlCommand = new SqlCommand();
-            sqlCommand.Connection = sqlConnection;
-            sqlCommand.CommandType = CommandType.Text;
-            sqlCommand.CommandText = sqlQuery;
-
-            // Execute the Query and get results
-            sqlConnection.Open();
-            SqlDataReader queryResults = sqlCommand.ExecuteReader();
-            while (queryResults.Read())
+            catch (NullReferenceException)
             {
-                lblDetails.Text = queryResults["Name"].ToString();
-                lblDetails1.Text = queryResults["Company"].ToString();
-                lblDetails2.Text = queryResults["Description"].ToString();
-                lblDetails3.Text = queryResults["Deadline"].ToString();
-                lblDetails4.Text = queryResults["Start"].ToString();
-                lblDetails5.Text = queryResults["Hours"].ToString();
-                lblDetails6.Text = queryResults["Payment"].ToString();
+                Session["MustLogin"] = "You must log in to access that page.";
+                Response.Redirect("/login/LoginPage.aspx");
+                throw;
             }
         }
 
