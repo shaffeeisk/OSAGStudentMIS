657cb5cd113fc652299f9ad3553a220f5349ca7e
Shaffee Iskandar <shaffeeisk@gmail.com>
2022-03-30 20:53:26 -0400

    REFINED BOOKMARKING

--- OSAG/internships/Internships.aspx.cs
+++ OSAG/internships/Internships.aspx.cs
@@ -9,13 +9,52 @@ using System.Data;
 using System.Data.SqlClient;
 //Connection Strings in web.config
 using System.Web.Configuration;
+
+
 namespace OSAG.internships
 {
     public partial class Internships : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Session["Username"] == null)
+            {
+                Session["MustLogin"] = "You must log in to access that page.";
+                Response.Redirect("/login/LoginPage.aspx");
+            }
 
+            // check each bookmark button and change text based on whether a bookmark exists
+            if (!IsPostBack) // only when first loading page
+            {
+                // define database connection
+                SqlConnection sqlConnection = new SqlConnection(WebConfigurationManager.ConnectionStrings["OSAG"].ConnectionString);
+                // Retrieve StudentID of user
+                String sqlQuery = "SELECT StudentID FROM Student WHERE UserName = '" + Session["Username"] + "'";
+                SqlCommand sqlCommand = new SqlCommand(sqlQuery, sqlConnection);
+                sqlConnection.Open();
+                int stuID = (int)sqlCommand.ExecuteScalar();
+                sqlConnection.Close();
+
+                // for each grid row
+                for (int i = 0; i < grdvwInternships.Rows.Count; i++)
+                {
+                    // define button to be changed and gridview row being used
+                    Button btn = (Button)grdvwInternships.Rows[i].FindControl("btnBookmark");
+                    GridViewRow gvr = (GridViewRow)btn.NamingContainer;
+
+                    // Retrieve InternshipID from row
+                    int itemID = (int)grdvwInternships.DataKeys[gvr.RowIndex]["InternshipID"];
+
+                    // check bookmark status
+                    sqlQuery = "SELECT COUNT(*) FROM InternshipMatch WHERE StudentID = " + stuID + " AND InternshipID = " + itemID + "AND IsBookmark = 1;";
+                    sqlCommand.CommandText = sqlQuery;
+                    sqlConnection.Open();
+                    int isBookmark = (int)sqlCommand.ExecuteScalar();
+                    sqlConnection.Close();
+                    if (isBookmark == 1)
+                        btn.Text = "Remove Bookmark";
+                }
+            }
         }
 
         protected void btnBookmark_Click(object sender, EventArgs e)
@@ -43,12 +82,21 @@ namespace OSAG.internships
 
             // Insert/Remove bookmark
             int[] matchRecord = getMatch(StudentID, InternshipID);
-            if (matchRecord == null)
+            if (matchRecord == null)        // add match record for bookmark
+            {
                 sqlQuery = "INSERT INTO InternshipMatch (IsBookmark, StudentID, InternshipID) VALUES (1, " + StudentID + ", " + InternshipID + ")";
-            else if (matchRecord[1] == 0)
+                btn.Text = "Remove Bookmark";
+            }
+            else if (matchRecord[1] == 0)   // set bookmarked in existing match record to true
+            {
                 sqlQuery = "UPDATE InternshipMatch SET IsBookmark = 1 WHERE InternshipMatchID = " + matchRecord[0] + ";";
-            else
+                btn.Text = "Remove Bookmark";
+            }
+            else                            // set bookmarked in existing match record to false
+            {
                 sqlQuery = "UPDATE InternshipMatch SET IsBookmark = 0 WHERE InternshipMatchID = " + matchRecord[0] + ";";
+                btn.Text = "Bookmark";
+            }
             sqlCommand.CommandText = sqlQuery;
             sqlConnection.Open();
             sqlCommand.ExecuteScalar();
