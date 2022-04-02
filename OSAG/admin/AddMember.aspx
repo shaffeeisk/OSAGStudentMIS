92fe0b038e3b9d5d1379371d37e593701ffc448a
Shaffee Iskandar <shaffeeisk@gmail.com>
2022-04-02 18:59:42 -0400

    ADD MEMBER (INCOMPLETE)

---
+++ OSAG/admin/AddMember.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/templates/Home.Master" AutoEventWireup="true" CodeBehind="AddMember.aspx.cs" Inherits="OSAG.admin.AddMember" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <br />
+    <br />
+    <strong>Create Member Account</strong>
+    <br />
+    <br />
+    Access Level:&nbsp
+            <asp:DropDownList ID="ddlMemberType" runat="server">
+                <asp:ListItem Text="Member" Value="4" />
+                <asp:ListItem Text="Mentor" Value="3" />
+                <asp:ListItem Text="Leadership" Value="2" />
+                <asp:ListItem Text="Admin" Value="1" />
+            </asp:DropDownList>
+    <br />
+    <br />
+    First Name:&nbsp
+            <asp:TextBox ID="txtFirstName" runat="server"></asp:TextBox>
+    <br />
+    <br />
+    Last Name:&nbsp
+            <asp:TextBox ID="txtLastName" runat="server"></asp:TextBox>
+    <br />
+    <br />
+    Email:&nbsp
+            <asp:TextBox ID="txtEmail" TextMode="Email" runat="server"></asp:TextBox>
+    <br />
+    <br />
+    Phone Number (Optional):&nbsp
+            <asp:TextBox ID="txtPhone" TextMode="Phone" runat="server"></asp:TextBox>
+    <br />
+    <br />
+    City:&nbsp
+            <asp:TextBox ID="txtCity" runat="server"></asp:TextBox>
+    <br />
+    <br />
+    State:&nbsp
+            <asp:TextBox ID="txtState" runat="server"></asp:TextBox>
+    <br />
+    <br />
+    <asp:Button ID="btnCreate" runat="server" Text="Create Member Account" OnClick="btnCreate_Click" />
+    <br />
+    <asp:Label ID="lblStatus" runat="server" Text=""></asp:Label>
+</asp:Content>
---
+++ OSAG/admin/AddMember.aspx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+// SQL Imports
+using System.Data.SqlClient;
+using System.Web.Configuration;
+using System.Drawing;
+
+namespace OSAG.admin
+{
+    public partial class AddMember : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Session["Username"] = "admin";
+            Session["UserType"] = "member";
+        }
+
+        // event handler for create member account button
+        protected void btnCreate_Click(object sender, EventArgs e)
+        {            
+            String sqlQuery = "INSERT INTO Member (MemberType, FirstName, LastName, Email, PhoneNumber, City, M_State, IsApproved) " +
+                "VALUES (@MemberType, @FirstName, @LastName, @Email, @PhoneNumber, @City, @M_State, 'FALSE')";
+            SqlConnection sqlConnect = new SqlConnection(WebConfigurationManager.ConnectionStrings["OSAG"].ConnectionString);
+            SqlCommand sqlCommand = new SqlCommand(sqlQuery, sqlConnect);
+            sqlCommand.Parameters.AddWithValue("@MemberType", ddlMemberType.SelectedValue);
+            sqlCommand.Parameters.AddWithValue("@FirstName", validate(txtFirstName.Text));
+            sqlCommand.Parameters.AddWithValue("@LastName", validate(txtLastName.Text));
+            sqlCommand.Parameters.AddWithValue("@Email", validate(txtEmail.Text));
+            sqlCommand.Parameters.AddWithValue("@PhoneNumber", validatePhone(txtPhone.Text));
+            sqlCommand.Parameters.AddWithValue("@City", validate(txtCity.Text));
+            sqlCommand.Parameters.AddWithValue("@M_State", validate(txtState.Text));
+            sqlConnect.Open();
+            sqlCommand.ExecuteScalar();
+            sqlConnect.Close();
+            ClearMemberData();
+            lblStatus.Text = "User successfully created.";
+        }
+
+        // helper method to clear user input
+        protected void ClearMemberData()
+        {
+            ddlMemberType.SelectedValue = "4";
+            txtFirstName.Text = "";
+            txtLastName.Text = "";
+            txtEmail.Text = "";
+            txtPhone.Text = "";
+            txtCity.Text = "";
+            txtState.Text = "";
+        }
+
+        // helper method to validate data. trims input string of leading/trailing white space.
+        // then returns null if user input is empty. otherwise, returns the trimmed string.
+        // allows data integrity to allow querying null input (also saves disk space :D)
+        // (e.g. display students who don't have graduation dates so member can help them figure out when they should graduate)
+        private object validate(String s)
+        {
+            s = s.Trim();
+            if (s == "")
+                return (object)DBNull.Value;
+            return s;
+        }
+
+        // helper method to return non-numeric stripped string from input
+        private string validatePhone(String i)
+        {
+            string o = new string(i.Where(c => char.IsDigit(c)).ToArray());
+            //if(o == "" || o.Length < 10)
+            //{
+            //    // MAYBE USE INPUT VALIDATOR INSTEAD FOR THIS, BUT USER SHOULD KNOW SOMETHING WENT WRONG
+            //    // OTHERWISE REMOVE STRING DECLARATION AND SIMPLY USE A ONE-LINE RETURN STATEMENT
+            //}
+            return o;
+        }
+    }
+}
