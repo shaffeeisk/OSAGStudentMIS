19d59f18306be0b6bfe8e5db4b0cf76068e41042
Shaffee Iskandar <shaffeeisk@gmail.com>
2022-04-03 22:00:03 -0400

    new webform for member to complete registration

---
+++ OSAG/login/CompleteRegistration.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/templates/Home.Master" AutoEventWireup="true" CodeBehind="CompleteRegistration.aspx.cs" Inherits="OSAG.login.CompleteRegistration" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+            Username:
+            <asp:TextBox ID="txtUsername" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Password:
+            <asp:TextBox ID="txtPassword" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="btnSubmit" runat="server" OnClick="btnSubmit_Click" Text="Submit" />
+            <br />
+            <asp:Label ID="lblStatus" runat="server"></asp:Label>
+            <br />
+            <br />
+            <asp:Button ID="btnContinue" runat="server" Text="Continue to profile" OnClick="btnContinue_Click" />
+</asp:Content>
---
+++ OSAG/login/CompleteRegistration.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OSAG.login
+{
+    public partial class CompleteRegistration : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["Username"] == null)
+                return;
+            if(Session["UserType"].ToString() != "member")
+            {
+                Session["AccessDenied"] = "You do not have access to that page.";
+                Response.Redirect("UserProfile.aspx");
+            }
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnContinue_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // run query, return number indiating whether user is approved
+        protected bool isApproved(String s)
+        {
+            String sqlQuery = "EXEC dbo.OSAG_NotApproved @Username";
+            SqlConnection sqlConnect = new SqlConnection(WebConfigurationManager.ConnectionStrings["OSAG"].ConnectionString);
+            SqlCommand sqlCommand = new SqlCommand(sqlQuery, sqlConnect);
+            sqlCommand.Parameters.AddWithValue("@Username", txtUsername.Text);
+            sqlConnect.Open();
+            int i = Convert.ToInt32(sqlCommand.ExecuteScalar());
+            sqlConnect.Close();
+
+            // return bool
+            if (i == 1)
+                return false;
+            return true;
+        }
+    }
+}
